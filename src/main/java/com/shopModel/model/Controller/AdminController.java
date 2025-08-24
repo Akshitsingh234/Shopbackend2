d9b96835ccebaf2433411be789ad2d8509f2c193
Akshitsingh234 <soniankush234@gmail.com>
2025-08-24 15:24:24 +0530

    Update API endpint 3000 to  netlify reverse

--- src/main/java/com/shopModel/model/Controller/AdminController.java
+++ src/main/java/com/shopModel/model/Controller/AdminController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
-@CrossOrigin(origins = "https://ornavalult.netlify.app")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
 
     @Autowired
